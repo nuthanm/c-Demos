4341d375a01557da7bf21d5c6988b53a29cc40b8
nuthan m <inbox.nuthan@gmail.com>
2021-07-24 20:03:18 +0530

    Added extra method to have message alone

--- ExtensionMethods/CustomLogger.cs
+++ ExtensionMethods/CustomLogger.cs
@@ -2,6 +2,11 @@ namespace System
 {
     public class CustomLogger
     {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
         public void Log(string message, string messageType)
         {
             Console.WriteLine($"{messageType}: {message}");
