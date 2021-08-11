4ac426481403eea0400fbd2bf48390839ecc64f2
nuthan m <inbox.nuthan@gmail.com>
2021-08-11 14:03:10 +0530

    Initial commit for Files and Streams

---
+++ FilesAndStreamDemos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilesAndStreamDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
