2a7a56ab2517c164204308ec15fa7b1bf61a6847
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2022-02-03 17:14:26 +0530

    Inheritence: Basic Base and Derived class demo

---
+++ Basic Programs/Inheritence_01.cs
@@ -0,0 +1,40 @@
+/*
+  Inheritence allows us to define a class(Derived Class) based on another class(Base Class).
+  Base class: The class whose properties(Base class properties) are inherited by another class (Derived class)
+  Derived class: The class which iherits the properties by itself
+  Ex: Animal (Base class)
+    cat & dog are derived class
+    cat & dog inherits Animal properties and methods, also each derived has its own properites and methods.
+*/
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Dog dog = new Dog();
+		dog.Bark();
+		
+    // Because we inherited Animal Base class to Dog Derived one. So that all properties of Base class can directly access from Derived
+		Console.WriteLine(dog.Legs); 		
+	}
+}
+
+class Animal{
+	public int Legs {get; set;}
+}
+
+class Dog : Animal{
+	public Dog(){
+		Legs = 4;
+	}
+	
+	public void DogLegs()
+	{
+		Console.WriteLine("Dog has {0} legs", Legs);
+	}
+	public void Bark()
+	{
+		Console.WriteLine("Woof");
+	}	
+}
