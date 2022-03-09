d57fb74f58d077951ce70cd08745efe4d995cae7
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2022-03-09 16:12:42 +0530

    Sample file which differentiates Parallel.ForEach vs traditional foreach
       Main Difference between Parallel.ForEach vs ForEach
       ForEach works on same thread and order of values in same.
       Parallel.ForEach works on multiple threads and order of values, not in the same way
       Execution time: traditional faster takes less than to complete when compared with Parallel.ForEach

---
+++ Parallel Execution/ParallelForeachVsForEach.cs
@@ -0,0 +1,38 @@
+/* Source I learned it from this video: https://www.youtube.com/watch?v=lzRKWzvLgME */
+/* Main Difference between Parallel.ForEach vs ForEach
+   ForEach works on same thread and order of values in same.
+   Parallel.ForEach works on multiple threads and order of values, not in the same way
+   Execution time: traditional faster takes less than to complete when compared with Parallel.ForEach
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class ParallelForEachVsForEach
+{
+   public static void Main()
+   {
+	   string[] names = {"Nani","Potti","Nuthan","Ramya"};
+	   
+	   // Using traditional foreach
+	   var sw = Stopwatch.StartNew();
+	   foreach(var name in names)
+	   {
+		   Console.WriteLine("Name:{0} and current thread:{1}", name, Thread.CurrentThread.ManagedThreadId);
+	   }
+	   Console.WriteLine("Total number of milliseconds to complete his process:{0}", sw.Elapsed.Milliseconds);
+	   
+	   // Using Parallel.ForEach
+	   sw = Stopwatch.StartNew();
+	   Parallel.ForEach(names, name =>
+	   {
+		   Console.WriteLine("Name:{0} and current thread:{1}", name, Thread.CurrentThread.ManagedThreadId);
+		   Thread.Sleep(10);
+	   });
+	   Console.WriteLine("Total number of milliseconds to complete his process:{0}", sw.Elapsed.Milliseconds);
+	   Console.ReadLine();
+   }
+}
